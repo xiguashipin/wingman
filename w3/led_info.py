885c88510edafc3aea91544598314b583e7c36f3
shipin <shipin.cn@gmail.com>
2019-12-03 20:09:49 -0500

    udpate to py3 version

--- w3/led_info.py
+++ w3/led_info.py
@@ -1,10 +1,4 @@
 from blinkstick import blinkstick
 
 for bstick in blinkstick.find_all():
-    print "Found device:"
-    print "    Manufacturer:  " + bstick.get_manufacturer()
-    print "    Description:   " + bstick.get_description()
-    print "    Serial:        " + bstick.get_serial()
-    print "    Current Color: " + bstick.get_color(color_format="hex")
-    print "    Info Block 1:  " + bstick.get_info_block1()
-    print "    Info Block 2:  " + bstick.get_info_block2()
+    print ('Serial: {}'.format(bstick.get_serial()))
